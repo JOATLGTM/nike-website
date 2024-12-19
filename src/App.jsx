e564b9067b46a52734f5fe177f778250eb93283e
Chris Vo <cv@Chriss-MacBook-Pro.local>
2024-12-19 14:40:35 -0700

    created a Nav to display links to other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import {
+	Hero,
+	PopularProducts,
+	CustomerReviews,
+	Footer,
+	Services,
+	Subscribe,
+	SuperQuality,
+	SpecialOffer,
+} from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => {
 	return (
 		<main className="relative">
-			Nav
+			<Nav />
 			<section className="xl:padding-1 wide:padding-r padding-b ">
-				Hero
+				<Hero />
 			</section>
-			<section className="padding">PopularProducts</section>
-			<section className="padding-x py-10">SuperProducts</section>
-			<section className="padding">Services</section>
-			<section className="padding">SpecialOffer</section>
-			<section className="padding bg-pale-blue">CustomerReviews</section>
+			<section className="padding">
+				<PopularProducts />
+			</section>
+			<section className="padding-x py-10">
+				<SuperQuality />
+			</section>
+			<section className="padding">
+				<Services />
+			</section>
+			<section className="padding">
+				<SpecialOffer />
+			</section>
+			<section className="padding bg-pale-blue">
+				<CustomerReviews />
+			</section>
 			<section className="padding-x sm:py-32 py-16 w-full">
-				Subscribe
+				<Subscribe />
 			</section>
 			<section className="padding bg-black padding-x padding-t pb-8">
-				Footer
+				<Footer />
 			</section>
 		</main>
 	);
