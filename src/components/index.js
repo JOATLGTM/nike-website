e8d71f30fa74897210ac5d5e63c3938342bd8e5e
Chris Vo <cv@Chriss-MacBook-Pro.local>
2024-12-26 22:17:59 -0700

    modified index file for better exporting

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Nav from "./Nav";
 import Button from "./Button";
 import PopularProductCard from "./PopularProductCard";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { ShoeCard, Button, Nav, PopularProductCard, ServiceCard };
+export { ShoeCard, Button, Nav, PopularProductCard, ServiceCard, ReviewCard };
