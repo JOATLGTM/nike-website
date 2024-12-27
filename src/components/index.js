48ccdd6075631ab11ad36b572983af45f6cdc059
Chris Vo <cv@Chriss-MacBook-Pro.local>
2024-12-26 21:19:45 -0700

    created SuperQuality section

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import ShoeCard from "./ShoeCard";
 import Nav from "./Nav";
 import Button from "./Button";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
 
-export { ShoeCard, Button, Nav, PopularProductCard };
+export { ShoeCard, Button, Nav, PopularProductCard, ServiceCard };
